9a2f3793265350d10e945593047d38e152a8f8e7
Harley-xk <harley.gb@foxmail.com>
2019-03-14 16:35:31 +0800

    修复 R.Swift 编译脚本

---
+++ {{cookiecutter.product_name}}/Sources/Models/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  {{cookiecutter.product_name}}
+//
+//  Created by Harley-xk on 2019/3/14.
+//  Copyright © 2019 {{cookiecutter.organization_name}}. All rights reserved.
+//
+
+import Foundation
+
+struct User: Codable {
+    
+    var name: String
+}
