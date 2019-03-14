01ab17de1afa96d8ca2606c29289aec89709e6a5
Harley-xk <harley.gb@foxmail.com>
2019-03-14 13:20:39 +0800

    add hooks to install pods

---
+++ hooks/post_gen_project.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+GREEN='\033[0;32m'
+echo -e "${GREEN}Project successfuly generated!"
+
+echo -e "${GREEN}Installing Pods..."
+
+pod install
+
+echo -e "${GREEN}Pods Install Finished"
