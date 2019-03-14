c31627d4acf98902c998a93efa80de37a9c29f74
Harley-xk <harley.gb@foxmail.com>
2019-03-14 12:43:08 +0800

    Initial commit

---
+++ {{product_name}}/Application Main/Configure.swift
@@ -0,0 +1,11 @@
+//
+//  Configure.swift
+//  {{product_name}}
+//
+//  Created by Harley-xk on 2019/3/14.
+//  Copyright © 2019 {{organization_name}}. All rights reserved.
+//
+
+import Foundation
+
+let apihost = "https://www.xxx.com/api"
---
+++ {{product_name}}/Sources/User Interface/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  {{product_name}}
+//
+//  Created by Harley-xk on 2019/3/14.
+//  Copyright © 2019 {{organization_name}} All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
