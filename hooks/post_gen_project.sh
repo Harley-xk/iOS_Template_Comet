5274bda959cac501cbc033070cc538850c4b7180
Harley-xk <harley.gb@foxmail.com>
2019-03-16 10:54:28 +0800

    add more info to post_gen script; open xcode after project generated

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
+DEFAULT='\033[0;39m'
+WHITE='\033[0;97m'
 GREEN='\033[0;32m'
-echo -e "${GREEN}Project successfuly generated!"
+LIGHTGREEN='\033[0;92m'
+CYAN='\033[0;36m'
+LIGHTCYAN='\033[0;96m'
 
-echo -e "${GREEN}Installing Pods..."
+echo -e "${GREEN}All files successfuly generated!"
+
+echo -e "${CYAN}Installing Pods...${DEFAULT}"
 
 pod install
 
-echo -e "${GREEN}Pods Install Finished"
+echo -e "${LIGHTGREEN}Project {{cookiecutter.product_name}} successfully generated!${DEFAULT}"
+
+echo -n "Open project with Xcode? (y/n) "
+
+read open
+
+if [ $open == "y" ]
+then
+open -a Xcode {{cookiecutter.product_name}}.xcworkspace
+echo -e "${LIGHTGREEN}Open project {{cookiecutter.product_name}} whit Xcode.${DEFAULT}"
+fi
